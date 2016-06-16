33a85a84e69a32e64599130e9082e4dbcdf92359
vivalalova <vivalalova0@gmail.com>
2016-06-17 03:23:57 +0800

    add 2 API
    POST  /users
    PUT    /users

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
+
+router.post('/', function(req, res, next) {
+    res.send('kerkekrker post');
+})
+
+router.put('/', function(req, res, next) {
+    res.send(404,'kerkekrker put');
+})
 
 module.exports = router;
