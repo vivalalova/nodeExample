a190369ee0f9fae41dcb48827e4aca1541262287
vivalalova <vivalalova0@gmail.com>
2016-06-17 03:38:00 +0800

    res.send 改為新寫法
    密碼加鹽然後hash

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+const passwordA = '12345678';
+const passwordB = 'abcdefg';
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
@@ -8,10 +12,14 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
     res.send('kerkekrker post');
+
+    var salt = bcrypt.genSaltSync(saltRounds);
+    var hash = bcrypt.hashSync(passwordA, salt);
+    console.log(hash)
 })
 
 router.put('/', function(req, res, next) {
-    res.send(404,'kerkekrker put');
+    res.status(404).send('kerkekrker put');
 })
 
 module.exports = router;
